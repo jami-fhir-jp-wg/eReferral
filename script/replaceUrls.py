8b337a0272457b9f36f836c50ae23b49f2cb034a
Kazuhiko Ohe <kohe@m.u-tokyo.ac.jp>
2023-04-16 17:41:12 +0900

    update python urils

--- script/replaceUrls.py
+++ script/replaceUrls.py
@@ -8,20 +8,24 @@
 # 例：python test2.py input.txt
 
 import sys
-
+import re
 with open(sys.argv[1], "r") as f:
     print("sys.argv[1]="+sys.argv[1]+" sys.argv[2]="+sys.argv[2], file=sys.stderr)
+    escapedVersion = sys.argv[2].replace(".","\.")
+    print(escapedVersion)
+
     for line in f:
-        sys.stderr.write(line)
+#        sys.stderr.write(line)
         if "https://simplifier.net/resolve?scope=jp-core.r4@1.1.1" in line:
-            sys.stderr.write(line)
-            line = line.replace("https://simplifier.net/resolve?scope=jp-core.r4@1.1.1-snap&amp;canonical=http://jpfhir.jp/fhir/core/StructureDefinition/",
-                                 "https://jpfhir.jp/fhir/core/"+sys.argv[2]+"StructureDefinition/").replace("_","-").replace("structuredefinition-","StructureDefinition-")
-            # 行末に　".html"を追加する。            
-            line = line.rstrip()
-            line = line + ".html"
-            line = line.lower()
-            sys.stderr.write(line)
+#            sys.stderr.write(line)
+            m = re.search('https:\/\/simplifier\.net\/resolve\?scope=jp-core\.r4@' + escapedVersion + '-snap&amp;canonical=http:\/\/jpfhir\.jp\/fhir\/core\/StructureDefinition\/(.*?)">(.*)$',line)
+            if m :
+                print("m.group(1)="+m.group(1),file=sys.stderr)
+                print("m.group(2)="+m.group(2),file=sys.stderr)
+                profileUrl = m.group(1).lower().replace("_","-")+".html"
+            line = line.replace("https://simplifier.net/resolve?scope=jp-core.r4@1.1.1-snap&amp;canonical=http://jpfhir.jp/fhir/core/StructureDefinition/"+m.group(1),
+                                 "https://jpfhir.jp/fhir/core/"+sys.argv[2]+"/StructureDefinition-"+profileUrl)
+
             print(line)
-        else:
-            print(line)
+#        else:
+#            print(line)
