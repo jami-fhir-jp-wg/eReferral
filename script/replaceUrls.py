bff03d9aeae5031e5355f2bbf445337f164404a3
Kazuhiko Ohe <kohe@m.u-tokyo.ac.jp>
2023-04-16 14:15:38 +0900

    python and replaceUrls

---
+++ script/replaceUrls.py
@@ -0,0 +1,23 @@
+# 入力ファイルの各行を読み込み、文字列"simplifier"が含まれる行について次のようなURL変換処理をする。
+# https://simplifier.net/resolve?scope=jp-core.r4@1.1.1-snap&amp;canonical=http://jpfhir.jp/fhir/core/StructureDefinition/JP_Observation_Common
+#    ↓
+# https://jpfhir.jp/fhir/core/1.1.1/StructureDefinition-jp-observation-common.html
+# その後、その行を出力する。
+# また、文字列"simpifier"が含まれない行については、そのまま出力する。
+# なお、入力ファイルは、コマンドライン引数で指定する。
+# 例：python test2.py input.txt
+
+import sys
+
+with open(sys.argv[1], "r") as f:
+    for line in f:
+        if "https://simplifier.net/resolve?scope=jp-core.r4@1.1.1" in line:
+            line = line.replace("https://simplifier.net/resolve?scope=jp-core.r4@1.1.1-snap&amp;canonical=http://jpfhir.jp/fhir/core/StructureDefinition/",
+                                 "https://jpfhir.jp/fhir/core/"+sys.argv[2]+"StructureDefinition/").replace("_","-").replace("structuredefinition-","StructureDefinition-")
+            # 行末に　".html"を追加する。            
+            line = line.rstrip()
+            line = line + ".html"
+            line = line.lower()
+            print(line)
+        else:
+            print(line)
