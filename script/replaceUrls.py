705ff973945e8e1cc480b4a795ed1c510ce3975a
Kazuhiko Ohe <kohe@m.u-tokyo.ac.jp>
2023-04-16 14:35:11 +0900

    sys.stderr.write(line)

--- script/replaceUrls.py
+++ script/replaceUrls.py
@@ -12,12 +12,16 @@ import sys
 with open(sys.argv[1], "r") as f:
     for line in f:
         if "https://simplifier.net/resolve?scope=jp-core.r4@1.1.1" in line:
+            sys.stderr.write(line)
             line = line.replace("https://simplifier.net/resolve?scope=jp-core.r4@1.1.1-snap&amp;canonical=http://jpfhir.jp/fhir/core/StructureDefinition/",
                                  "https://jpfhir.jp/fhir/core/"+sys.argv[2]+"StructureDefinition/").replace("_","-").replace("structuredefinition-","StructureDefinition-")
             # 行末に　".html"を追加する。            
             line = line.rstrip()
             line = line + ".html"
             line = line.lower()
+            sys.stderr.write(line)
             print(line)
         else:
             print(line)
+            # 標準エラー
+            # sys.stderr.write(line)
